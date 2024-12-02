b6730cb55087a6831456c038b34aa2c605302a4c
Rahil Azhar <rahil.azhar10.com>
2024-12-02 10:17:55 +0500

    first commit

--- server/server.js
+++ server/server.js
@@ -1,52 +1,58 @@
 // server.js
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import categoryRoutes from './routes/categoryRoutes.js';
-import instructorRoutes from './routes/instructorRoutes.js';
-import courseRoutes from './routes/courseRoutes.js';
-import cors from 'cors';
-import blogpostrouter from './routes/blogpostroutes.js';
-import blogcatroutes from './routes/Blogcatroutes.js';
-import specialBroutes from './routes/special.blogPostRoutes.js';
-import specialcityroutes from './routes/cityCategoryRoutes.js';
-import router from './routes/eflyerRoutes.js';
-import routerfaq from './routes/faqCategoryRoutes.js';
-import routerquestion from './routes/questionRoutes.js';
-import routergallery from './routes/galleryCategoryRoutes.js';
-import freetrailrouter from './routes/freetrialroutes.js';
-import eventrouter from './routes/eventCategoryRoutes.js';
-import Everouter from './routes/eventRoutes.js';
-import modelroutes from './routes/coursemodel.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import instructorRoutes from "./routes/instructorRoutes.js";
+import courseRoutes from "./routes/courseRoutes.js";
+import cors from "cors";
+import blogpostrouter from "./routes/blogpostroutes.js";
+import blogcatroutes from "./routes/Blogcatroutes.js";
+import specialBroutes from "./routes/special.blogPostRoutes.js";
+import specialcityroutes from "./routes/cityCategoryRoutes.js";
+import router from "./routes/eflyerRoutes.js";
+import routerfaq from "./routes/faqCategoryRoutes.js";
+import routerquestion from "./routes/questionRoutes.js";
+import routergallery from "./routes/galleryCategoryRoutes.js";
+import freetrailrouter from "./routes/freetrialroutes.js";
+import eventrouter from "./routes/eventCategoryRoutes.js";
+import Everouter from "./routes/eventRoutes.js";
+import modelroutes from "./routes/coursemodel.js";
 // Load environment variables
 dotenv.config();
 // Connect to the database
 connectDB();
 const app = express();
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 app.use(express.json({ limit: "10mb" }));
 // CORS Middleware Configuration
-app.use(cors({
-    origin: 'http://localhost:5173', // Allow requests from this origin (frontend)
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow requests from this origin (frontend)
     credentials: true, // Enable sending cookies and credentials
-    methods: 'GET, POST, PUT, DELETE, OPTIONS', // Allow these methods
-    allowedHeaders: 'Content-Type, Authorization', // Allowed headers
-}));
+    methods: "GET, POST, PUT, DELETE, OPTIONS", // Allow these methods
+    allowedHeaders: "Content-Type, Authorization", // Allowed headers
+  })
+);
+
+app.get("/", (req, res) => {
+  return res.send({ Message: "Hellow World" });
+});
 // API routes
-app.use('/api/categories',categoryRoutes);
-app.use('/api/instructors',instructorRoutes);
-app.use('/api/courses',courseRoutes);
-app.use('/api/blogpost',blogpostrouter);
-app.use('/api/blogcate',blogcatroutes);
-app.use('/api/specialcatblog',specialBroutes);
-app.use('/api/citycategory',specialcityroutes);
-app.use('/api/eflyer',router);
-app.use('/api/faqcat',routerfaq);
-app.use('/api/faquestion',routerquestion);
-app.use('/api/gallery',routergallery);
-app.use('/api/freetrial',freetrailrouter);
-app.use('/api/event',eventrouter);
-app.use('/api/coursemodel',modelroutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/instructors", instructorRoutes);
+app.use("/api/courses", courseRoutes);
+app.use("/api/blogpost", blogpostrouter);
+app.use("/api/blogcate", blogcatroutes);
+app.use("/api/specialcatblog", specialBroutes);
+app.use("/api/citycategory", specialcityroutes);
+app.use("/api/eflyer", router);
+app.use("/api/faqcat", routerfaq);
+app.use("/api/faquestion", routerquestion);
+app.use("/api/gallery", routergallery);
+app.use("/api/freetrial", freetrailrouter);
+app.use("/api/event", eventrouter);
+app.use("/api/coursemodel", modelroutes);
 // Start the server
 
 // Start the server
